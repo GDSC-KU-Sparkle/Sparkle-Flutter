e0c5604d9bb82c55b2702bd26cdbdbcfc618ac33
Sung Chul Park <jasonsc@naver.com>
2024-02-20 11:03:24 +0900

    feat : goal detail screen layout, style modified

--- lib/screen/goal_detail_screen.dart
+++ lib/screen/goal_detail_screen.dart
@@ -8,85 +8,131 @@ class GoalDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+
     return Container(
+      height: height - 300,
       padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
       child: Container(
-        color: Colors.amber,
+        // padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black87,
+              spreadRadius: 3,
+              offset: Offset(1, 1), // changes position of shadow
+            ),
+          ],
+        ),
         child: Column(children: [
-          Image.asset("assets/images/example.png"),
-          Container(
-            child: Column(mainAxisSize: MainAxisSize.min, children: [
-              Text("목표명"),
-              Text("자세한 설명 쏼라쏼라"),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("예상 시간 50분 - 60분"),
-                  Row(
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.asset(
+              "assets/images/example.png",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(children: [
+                Text("목표명"),
+                SizedBox(
+                  height: 10,
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      GestureDetector(
-                        child: Container(
-                          padding:
-                              EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-                          decoration: BoxDecoration(
-                            color: Colors.red,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black87,
-                                spreadRadius: 3,
-                                offset:
-                                    Offset(1, 1), // changes position of shadow
-                              ),
+                      Container(
+                        constraints: BoxConstraints(
+                          maxHeight: 80,
+                        ),
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              Text(
+                                "자세한 설명 쏼라쏼라 쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라자세한 설명 쏼라쏼라 쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라자세한 설명 쏼라쏼라 쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라자세한 설명 쏼라쏼라 쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라자세한 설명 쏼라쏼라 쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라 ",
+                              )
                             ],
-                          ),
-                          child: Text(
-                            "시작",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 14,
-                              color: Colors.white,
-                            ),
                           ),
                         ),
                       ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      GestureDetector(
-                        child: Container(
-                          padding:
-                              EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-                          decoration: BoxDecoration(
-                            color: Color(PrimaryColor[70]!),
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black87,
-                                spreadRadius: 3,
-                                offset:
-                                    Offset(1, 1), // changes position of shadow
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("예상 시간 50분 - 60분"),
+                          Row(
+                            children: [
+                              GestureDetector(
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 15, vertical: 5),
+                                  decoration: BoxDecoration(
+                                    color: Colors.red,
+                                    borderRadius: BorderRadius.circular(10),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.black87,
+                                        spreadRadius: 3,
+                                        offset: Offset(
+                                            1, 1), // changes position of shadow
+                                      ),
+                                    ],
+                                  ),
+                                  child: Text(
+                                    "시작",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 14,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
                               ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              GestureDetector(
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 15, vertical: 5),
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey,
+                                    borderRadius: BorderRadius.circular(10),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.black87,
+                                        spreadRadius: 3,
+                                        offset: Offset(
+                                            1, 1), // changes position of shadow
+                                      ),
+                                    ],
+                                  ),
+                                  child: Text(
+                                    "나중에",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 14,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                                onTap: () {
+                                  context.pop();
+                                },
+                              )
                             ],
                           ),
-                          child: Text(
-                            "자세히",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 14,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                        onTap: () {
-                          context.go("/goals/10000");
-                        },
+                        ],
                       )
                     ],
                   ),
-                ],
-              )
-            ]),
+                )
+              ]),
+            ),
           )
         ]),
       ),
